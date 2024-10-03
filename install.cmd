47e842e9bc982822ffddc4ad606a33cf91144938
Turbobot <turbobot@vercel.com>
2024-10-03 07:07:40 +0530

    feat(create-turbo): apply official-starter transform

--- install.cmd
+++ install.cmd
@@ -1,3 +1,8 @@
 @echo off
-npm install
-npm run build
+echo Cleaning up old dependencies...
+bun run rimraf node_modules **/node_modules
+
+echo Installing dependencies...
+bun install
+
+echo Setup complete!
